54ccddc5ddd0bebeba3aa878b850e8d5b6baeae8
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2025-07-24 20:21:29 -0700

    Change redirects to push to palette list

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,13 @@ use App\Http\Controllers\PaletteController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [PaletteController::class, 'index'])->name('palette');
-Route::get('/palette/new', [PaletteController::class, 'create']);
-Route::post('/palette/new', [PaletteController::class, 'store'])->name('palette');
-Route::get('/palette/{palette}', [PaletteController::class, 'show'])->name('palette');
-Route::delete('/palette/{palette}', [PaletteController::class, 'destroy'])->name('palette');
+Route::get('/', [PaletteController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('/palette/new', [PaletteController::class, 'create']);
+    Route::post('/palette/new', [PaletteController::class, 'store'])->name('palette');
+    Route::get('/palette/{palette}', [PaletteController::class, 'show'])->name('palette');
+    Route::delete('/palette/{palette}', [PaletteController::class, 'destroy'])->name('palette');
 
 });
 
