8df12eaa15a033c5303d2c18cd756cb8eb36463c
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2025-07-24 20:34:01 -0700

    Update route names and fix route permission
    - Fix route permission for creating palette
    - Remove unneeded names on route

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,14 @@
 
 use App\Http\Controllers\PaletteController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', [PaletteController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/palette/new', [PaletteController::class, 'create']);
-    Route::post('/palette/new', [PaletteController::class, 'store'])->name('palette');
-    Route::get('/palette/{palette}', [PaletteController::class, 'show'])->name('palette');
-    Route::delete('/palette/{palette}', [PaletteController::class, 'destroy'])->name('palette');
+    Route::get('/palette/new', [PaletteController::class, 'create'])->name('create')->can('create', 'App\Models\User');
+    Route::post('/palette/new', [PaletteController::class, 'store']);
+    Route::get('/palette/{palette}', [PaletteController::class, 'show']);
+    Route::delete('/palette/{palette}', [PaletteController::class, 'destroy']);
 
 });
 
