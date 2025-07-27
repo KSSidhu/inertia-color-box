5269f797a925405ccda5daad327520dab2d9a775
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2025-07-26 22:39:21 -0700

    Fix update FE logic
    - Replace link with a button
    - Force reload after redirect following delete

--- resources/js/components/palette/delete-palette-dialog.tsx
+++ resources/js/components/palette/delete-palette-dialog.tsx
@@ -1,8 +1,9 @@
-import { Link } from "@inertiajs/react"
+import { useForm } from "@inertiajs/react"
 import CheckIcon from "@mui/icons-material/Check"
 import CloseIcon from "@mui/icons-material/Close"
 import {
   Avatar,
+  Button,
   Dialog,
   DialogTitle,
   List,
@@ -21,24 +22,22 @@ interface Props {
 
 export default function DeletePaletteDialog({ deletingId, isOpen, onClose }: Props) {
   const { classes } = useStyles()
+  const form = useForm<{ paletteId: string }>({
+    paletteId: deletingId || "",
+  })
 
   return (
     <Dialog open={isOpen}>
       <DialogTitle>{"Delete this palette?"}</DialogTitle>
       <List>
-        <Link
-          onClick={removePalette}
-          href={`/palette/${deletingId}`}
-          method={"delete"}
-          className={classes.confirm}
-        >
+        <Button onClick={removePalette} className={classes.confirm}>
           <ListItemAvatar>
             <Avatar style={{ backgroundColor: blue[100], color: blue[600] }}>
               <CheckIcon />
             </Avatar>
           </ListItemAvatar>
           <span>{"Delete"}</span>
-        </Link>
+        </Button>
         <ListItemButton onClick={onClose}>
           <ListItemAvatar>
             <Avatar style={{ backgroundColor: red[100], color: red[600] }}>
@@ -52,6 +51,11 @@ export default function DeletePaletteDialog({ deletingId, isOpen, onClose }: Pro
   )
 
   function removePalette() {
+    if (!deletingId) return
+    // Don't preserve state to force reload
+    form.delete(`/palette/${deletingId}`, {
+      preserveState: false,
+    })
     onClose()
   }
 }
@@ -60,6 +64,7 @@ const useStyles = makeStyles()((theme) => ({
   confirm: {
     display: "flex",
     alignItems: "center",
+    justifyContent: "flex-start",
     padding: theme.spacing(2),
     width: "100%",
     "&:hover": {
